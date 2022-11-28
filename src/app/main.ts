5abf49e8618de39e9508cbe3ab4cdbfa7a12ffbc
Jabgomez <josebaquerizo_1999@hotmail.com>
2022-11-28 00:48:00 -0500

    Update Create product with Omit DTO

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,7 +3,6 @@ import { faker } from '@faker-js/faker';
 
 for(let i = 0; i < 50; i++) {
     addProduct({
-        id: faker.datatype.uuid(),
         description: faker.commerce.productDescription(),
         image: faker.image.imageUrl(),
         color: faker.color.human(),
@@ -11,16 +10,9 @@ for(let i = 0; i < 50; i++) {
         isNew: faker.datatype.boolean(),
         tags: faker.helpers.arrayElements(),
         title: faker.commerce.productName(),
-        createdAt: faker.date.recent(),
-        updatedAt: faker.date.recent(),
         stock: faker.datatype.number({ min: 10, max: 100}),
         size: faker.helpers.arrayElement(['M', 'S', 'XL', 'L']),
-        category: {
-            id: faker.datatype.uuid(),
-            name: faker.commerce.department(),
-            createdAt: faker.date.recent(),
-            updatedAt: faker.date.recent(),
-        }
+        categoryId: faker.datatype.uuid(),
     })
 }
 
